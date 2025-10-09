334cad723889949b7b1f5ed374e75b0e64fc9f1e
Srijan S Uppoor <srijanuppoor@gmail.com>
2025-10-09 21:46:43 +0530

    Update async_fifo_test.sv

--- src/async_fifo_test.sv
+++ src/async_fifo_test.sv
@@ -1,4 +1,5 @@
 class async_fifo_test extends uvm_test;
+  
   `uvm_component_utils(async_fifo_test)
 
   async_fifo_env env;
@@ -6,7 +7,7 @@ class async_fifo_test extends uvm_test;
 
   function new(string name="async_fifo_test", uvm_component parent=null);
     super.new(name, parent);
-  endfunction
+  endfunction:new
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
@@ -14,7 +15,7 @@ class async_fifo_test extends uvm_test;
     
     uvm_config_db#(uvm_active_passive_enum)::set(this,"env.ragent" , "is_active" ,UVM_ACTIVE);      		  
     uvm_config_db#(uvm_active_passive_enum)::set(this,"env.wagent","is_active",UVM_ACTIVE);
-  endfunction
+  endfunction:build_phase
 
   task run_phase(uvm_phase phase);
     super.run_phase(phase);
@@ -22,7 +23,8 @@ class async_fifo_test extends uvm_test;
     vs = async_fifo_virtual_sequence::type_id::create("vs");
     repeat(1000)begin
       vs.start(env.vseqr);
-    end
+    end:repeat
     phase.drop_objection(this);
-  endtask
-endclass
+  endtask:run_phase
+  
+endclass:async_fifo_test
