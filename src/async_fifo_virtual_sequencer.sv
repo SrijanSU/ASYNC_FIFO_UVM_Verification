7cb4814cb6ff71dc114c0b14e1a5b22aa14a6f86
Srijan S Uppoor <srijanuppoor@gmail.com>
2025-10-10 21:32:36 +0530

    Update async_fifo_virtual_sequencer.sv Added Comments

--- src/async_fifo_virtual_sequencer.sv
+++ src/async_fifo_virtual_sequencer.sv
@@ -1,10 +1,25 @@
+//============================================================
+// Project      : Asynchronous FIFO Verification
+// File Name    : async_fifo_virtual_sequencer.sv
+// Description  : Virtual sequencer that connects the write and 
+//                read sequencers, allowing coordination of 
+//                their sequences in the Asynchronous FIFO testbench.
+// Author       : Srijan S Uppoor
+//============================================================
+
 class async_fifo_virtual_sequencer extends uvm_sequencer;
   
-  `uvm_component_utils(async_fifo_virtual_sequencer)
-  
+  `uvm_component_utils(async_fifo_virtual_sequencer)    //Register the component with factory
+
+  // --------------------------------------------------------
+  // Handles to lower-level sequencers
+  // --------------------------------------------------------
   async_fifo_write_sequencer write_seqr;
   async_fifo_read_sequencer  read_seqr;
- 
+
+  // --------------------------------------------------------
+  // Constructor
+  // --------------------------------------------------------
   function new(string name="async_fifo_virtual_sequencer", uvm_component parent=null);
     super.new(name, parent);
   endfunction:new
