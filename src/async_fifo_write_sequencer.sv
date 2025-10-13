dbdf631f9492d1c94febc4d7ea8de7c7dcbc8b90
Srijan S Uppoor <srijanuppoor@gmail.com>
2025-10-13 08:50:11 +0530

    Update async_fifo_write_sequencer.sv Added Comments

--- src/async_fifo_write_sequencer.sv
+++ src/async_fifo_write_sequencer.sv
@@ -1,7 +1,18 @@
+//============================================================
+// Project      : Asynchronous FIFO Verification
+// File Name    : async_fifo_write_sequencer.sv
+// Description  : Sequencer for generating write-side sequence
+//                items to be sent to the FIFO write driver.
+// Author       : Srijan S Uppoor
+//============================================================
+
 class async_fifo_write_sequencer extends uvm_sequencer #(async_fifo_write_item);
   
-  `uvm_component_utils(async_fifo_write_sequencer)
-  
+  `uvm_component_utils(async_fifo_write_sequencer)        // Registering with Factory
+
+  // --------------------------------------------------------
+  // Constructor
+  // --------------------------------------------------------
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction:new
